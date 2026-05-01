3d881ff366aa48f3264759c0bc3aeecd01ff2b08
Abubakar M Aji <zannahmuhammadaji@gmail.com>
2026-05-01 09:47:12 +0100

    chore: move image field to bottom of callToAction schema

--- src/sanity/schemaTypes/callToActionType.ts
+++ src/sanity/schemaTypes/callToActionType.ts
@@ -2,10 +2,10 @@ import { defineField, defineType } from 'sanity'
 export const callToActionType = defineType({
   name: 'callToAction', title: 'Call To Action', type: 'document',
   fields: [
-    defineField({ name: 'image', title: 'Image', type: 'image', options: { hotspot: true } }),
     defineField({ name: 'heading', title: 'Heading', type: 'string' }),
     defineField({ name: 'description', title: 'Description', type: 'text' }),
     defineField({ name: 'primaryLabel', title: 'Button Label', type: 'string' }),
     defineField({ name: 'primaryHref', title: 'URL Link', type: 'string' }),
+    defineField({ name: 'image', title: 'Image', type: 'image', options: { hotspot: true } }),
   ],
 })
